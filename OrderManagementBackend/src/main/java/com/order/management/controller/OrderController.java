6ffa800758220ce0837eb9892447a60add6cf3af
Cameron Aumann <1000074183@hexaware.com>
2022-07-01 11:38:13 -0400

    added console logging

--- OrderManagementBackend/src/main/java/com/order/management/controller/OrderController.java
+++ OrderManagementBackend/src/main/java/com/order/management/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.order.management.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,11 +22,13 @@ import com.order.management.model.Order;
 import com.order.management.model.Product;
 import com.order.management.service.OrderService;
 
-//@Slf4j
 @RestController
 @RequestMapping("/v1")
 @CrossOrigin(origins = { "http://localhost:8080", "http://127.0.0.1:5500" })
 public class OrderController {
+	
+	Logger log = LoggerFactory.getLogger(OrderController.class);
+	
 	
 	private OrderService service;
 	
@@ -36,14 +41,15 @@ public class OrderController {
 	@GetMapping("/orders") //working
 	@ResponseStatus(HttpStatus.OK)
 	public List<Order> getOrders() {
+		log.info("REQUEST: " + "GET /orders @ " + LocalDateTime.now());
 		return service.getOrders();
 	}
 	
 	@PostMapping("/orders/new") //working
 	@ResponseStatus(HttpStatus.CREATED)
 	public void newOrder(/*@RequestBody Order order*/) {
+		log.info("REQUEST: " + "POST /orders/new @ " + LocalDateTime.now());
 		service.newOrder(new Order());
-//		service.newOrder(order);
 	}
 	
 	@Deprecated //no need to update the order ; obsolete
@@ -51,8 +57,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.OK)
 	public void updateOrder(@RequestBody Order order) {
 		if (order.getId() == 0 || order.getCreated() == null) {
+			log.info("REQUEST: " + "ERROR PUT /orders/update @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing order details");
 		} else {
+			log.info("REQUEST: " + "PUT /orders/update @ " + LocalDateTime.now());
 			service.updateOrder(order);
 		}
 	}
@@ -61,8 +69,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteOrder(@RequestBody Order order) {
 		if (order.getId() == 0) {
+			log.info("REQUEST: " + "ERROR DELETE /orders/delete @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing order details");
 		} else {
+			log.info("REQUEST: " + "DELETE /orders/delete @ " + LocalDateTime.now());
 			service.deleteOrder(order);
 		}
 	}
@@ -71,8 +81,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<Product> getProductsByOrder(@RequestBody Order order) {
 		if (order.getId() == 0 || order.getId() == null || order.getCreated() == null) {
+			log.info("REQUEST: " + "ERROR GET /products @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing order details");
 		}
+		log.info("REQUEST: " + "GET /products @ " + LocalDateTime.now());
 		return service.getProductsByOrder(order);
 	}
 	
@@ -81,8 +93,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void addProduct(@RequestBody Product product) {
 		if (product.getName() == null || product.getPrice() == 0 || product.getManufacturer() == null || product.getManufacturer() == "") {
+			log.info("REQUEST: " + "ERROR POST /products/new @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing product details");
 		} else {
+			log.info("REQUEST: " + "POST /products/new @ " + LocalDateTime.now());
 			service.newProduct(product);
 		}
 	}
@@ -91,8 +105,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.OK)
 	public void updateProduct(@RequestBody Product product) {
 		if (product.getId() == 0 || product.getId() == null || product.getName() == null || product.getPrice() == 0 || product.getManufacturer() == null || product.getManufacturer() == "") {
+			log.info("REQUEST: " + "ERROR PUT /products/update @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing product details");
 		} else {
+			log.info("REQUEST: " + "PUT /products/update @ " + LocalDateTime.now());
 			service.updateProduct(product);
 		}
 	}
@@ -101,8 +117,10 @@ public class OrderController {
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteProduct(@RequestBody Product product) {
 		if (product.getId() == 0 || product.getId() == null || product.getName() == null || product.getPrice() == 0 || product.getManufacturer() == null || product.getManufacturer() == "") {
+			log.info("REQUEST: " + "ERROR DELETE /products/delete @ " + LocalDateTime.now());
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing product details");
 		} else {
+			log.info("REQUEST: " + "DELETE /products/delete @ " + LocalDateTime.now());
 			service.deleteProduct(product);
 		}
 	}
